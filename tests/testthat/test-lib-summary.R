d7a39198e4162c8679853da94232c676afd070cb
jenfransson <fransson.jen@gmail.com>
2024-06-14 12:18:21 +0200

    Include size argument to lib_summary

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,12 +1,23 @@
-test_that("lib_summary returns expected results", {
-  res <- lib_summary()
+test_that("lib_summary(TRUE) returns expected results", {
+  res <- lib_summary(TRUE)
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 3)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+  expect_type(res$lib_size, "double")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary(sizes = "foo"), "not interpretable as logical")
+})
+
+
+test_that("lib_summary(FALSE) returns expected results", {
+  res <- lib_summary(FALSE)
   expect_s3_class(res, "data.frame")
   expect_equal(ncol(res), 2)
   expect_equal(names(res), c("Library", "n_packages"))
   expect_type(res$Library, "character")
   expect_type(res$n_packages, "integer")
-})
-
-test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
 })
